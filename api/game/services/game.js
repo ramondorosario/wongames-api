ce7a38ba5d0e28da490d3497a2da074c8419e699
ramondorosario <ramondorosario@gmail.com>
2021-08-30 22:31:50 -0300

    feat: create a function that returns the game's description and rating

--- api/game/services/game.js
+++ api/game/services/game.js
@@ -7,6 +7,23 @@
 
 const axios = require("axios");
 
+async function getGameInfo(slug) {
+  const jsdom = require("jsdom");
+  const { JSDOM } = jsdom;
+  const body = await axios.get(`https://www.gog.com/game/${slug}`);
+  const dom = new JSDOM(body.data);
+
+  const description = dom.window.document.querySelector(".description");
+
+  return {
+    rating: "BR0",
+    short_description: description.textContent.slice(0, 160),
+    description: description.innerHTML,
+  };
+}
+
+const body = axios;
+
 module.exports = {
   populate: async (params) => {
     const gogUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`;
@@ -15,6 +32,6 @@ module.exports = {
       data: { products },
     } = await axios.get(gogUrl);
 
-    console.log(products);
+    console.log(await getGameInfo(products[0].slug));
   },
 };
