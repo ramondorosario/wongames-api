68b602bc67d2b26f14874d6e77d58910388c8ee3
ramondorosario <ramondorosario@gmail.com>
2021-08-30 21:30:58 -0300

    feat: create controller to games/populate

--- api/game/controllers/game.js
+++ api/game/controllers/game.js
@@ -1,8 +1,16 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  populate: async (ctx) => {
+    console.log("Starting to populate...");
+
+    await strapi.services.game.populate();
+
+    ctx.send("Finished populating!");
+  },
+};
